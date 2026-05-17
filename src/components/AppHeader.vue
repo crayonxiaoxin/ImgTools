9f1eb40c7ec5a84cb496da673fbbe1eb62246b18
crayonxiaoxin <rmbee@qq.com>
2026-05-17 17:57:39 +0800

    feat: add version display with package.json import

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import { version } from '../../package.json'
 </script>
 
 <template>
   <header class="app-header">
     <h1 class="logo">ImgTools</h1>
     <p class="tagline">浏览器端图片处理工具</p>
+    <span class="version">v{{ version }}</span>
   </header>
 </template>
 
@@ -27,5 +29,10 @@
   font-size: 12px;
   color: #999;
   margin: 0;
+}
+.version {
+  font-size: 11px;
+  color: #bbb;
+  margin-left: auto;
 }
 </style>
